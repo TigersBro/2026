a56cd32da6767c4a2fc7bf5ebb3133d183ef3a7f
TigersBro <159957926+TigersBro@users.noreply.github.com>
2026-02-23 15:09:23 -0500

    Update Constants.java
    Modify leader and followers for drivetrain.

--- DugE/src/main/java/frc/robot/Constants.java
+++ DugE/src/main/java/frc/robot/Constants.java
@@ -23,8 +23,8 @@ public final class Constants {
   public static final class DriveConstants {
     // Motor controller IDs for drivetrain motors
     public static final int LEFT_LEADER_ID = 1;
-    public static final int LEFT_FOLLOWER_ID = 3;
-    public static final int RIGHT_LEADER_ID = 2;
+    public static final int LEFT_FOLLOWER_ID = 2;
+    public static final int RIGHT_LEADER_ID = 3;
     public static final int RIGHT_FOLLOWER_ID = 4;
 
     public static final double SLOW_MODE_MOVE = 0.55;
