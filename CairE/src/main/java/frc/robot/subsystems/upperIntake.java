cb139454e92c0fcd52a245bab0605f2827c29ff0
TigersBro <159957926+TigersBro@users.noreply.github.com>
2026-03-16 17:18:03 -0400

    Update upperIntake.java

--- CairE/src/main/java/frc/robot/subsystems/upperIntake.java
+++ CairE/src/main/java/frc/robot/subsystems/upperIntake.java
@@ -4,11 +4,39 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class upperIntake extends SubsystemBase {
   /** Creates a new upperIntake. */
-  public upperIntake() {}
+
+  private boolean isStalled;
+  private boolean isReset;
+  private final SparkMax Intake;
+
+  public upperIntake() {
+
+    isStalled = false;
+    isReset = false;
+    SparkMaxConfig intakeConfig = new SparkMaxConfig();
+    Intake = new SparkMax(Constants.IntakeConstants.upperIntakeID, MotorType.kBrushless);
+    intakeConfig.smartCurrentLimit(Constants.IntakeConstants.INDEXER_MOTOR_CURRENT_LIMIT);
+    intakeConfig.inverted(true);
+    Intake.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+
+
+
+  }
+
+  
+
 
   @Override
   public void periodic() {
